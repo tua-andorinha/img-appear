41407c1bfe78485c15c3df9e027197fa8e3c7246
tua-andorinha <155818724+tua-andorinha@users.noreply.github.com>
2024-02-24 03:42:49 -0800

    Add files via upload

---
+++ imgappear.js
@@ -0,0 +1,6 @@
+var catCase = document.querySelector(".show-cat");
+//console.log("catCase");
+var cat = document.querySelector(".cat");
+addEventListener("click", function () {
+  cat.classList.add("show");
+});
